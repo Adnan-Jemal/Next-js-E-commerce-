762e0bcf0963a3b96f381f781ae4cd662530c7af
adn-abdu <134360306+Adnan-jemal@users.noreply.github.com>
2024-02-11 20:35:32 +0300

    preparing for deploymnet

--- context/cartContext.tsx
+++ context/cartContext.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { createContext, useEffect, useState } from "react";
 
 type cartItemsType = {
@@ -8,6 +7,7 @@ type cartItemsType = {
   price: number;
   img: string;
 };
+
 type cartContexType = {
   cartItems: cartItemsType[];
   AddCartItem: (item: cartItemsType) => void;
@@ -27,33 +27,38 @@ import React from "react";
 const CartWrapper = ({ children }: { children: React.ReactNode }) => {
   const CART_STORAGE_KEY = "CartItems";
 
-  const [cartItems, setCartItems] = useState<cartItemsType[]>(() => {
-    const storedCartItems = localStorage.getItem(CART_STORAGE_KEY);
-    return storedCartItems ? JSON.parse(storedCartItems) : [];
-  });
+  const [cartItems, setCartItems] = useState<cartItemsType[]>([]);
 
   useEffect(() => {
-    const storedCartItems = localStorage.getItem(CART_STORAGE_KEY);
-    if (storedCartItems) {
-      setCartItems(JSON.parse(storedCartItems));
+    // Check if localStorage is available
+    if (typeof window !== "undefined") {
+      const storedCartItems = localStorage.getItem(CART_STORAGE_KEY);
+      setCartItems(storedCartItems ? JSON.parse(storedCartItems) : []);
     }
   }, []);
 
-  useEffect(() => {
-    localStorage.setItem(CART_STORAGE_KEY, JSON.stringify(cartItems));
-  }, [cartItems]);
-
   const AddCartItem = (item: cartItemsType) => {
     setCartItems([...cartItems, item]);
+    // Update localStorage when adding an item
+    localStorage.setItem(
+      CART_STORAGE_KEY,
+      JSON.stringify([...cartItems, item])
+    );
   };
+
   const ClearCartItems = () => {
     setCartItems([]);
+    // Remove localStorage when clearing cart
     localStorage.removeItem(CART_STORAGE_KEY);
-    
-    
   };
-  const RemoveCartItem = (itmeId: string) => {
-    setCartItems(cartItems.filter((item) => item.id != itmeId));
+
+  const RemoveCartItem = (itemId: string) => {
+    setCartItems(cartItems.filter((item) => item.id !== itemId));
+    // Update localStorage when removing an item
+    localStorage.setItem(
+      CART_STORAGE_KEY,
+      JSON.stringify(cartItems.filter((item) => item.id !== itemId))
+    );
   };
 
   return (
