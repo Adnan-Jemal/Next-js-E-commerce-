2c76a802c86d0fabd4c8c6eff413a4e416122c7b
adn-abdu <134360306+Adnan-jemal@users.noreply.github.com>
2024-02-11 19:54:07 +0300

    fixed error for deployment

--- app/checkout/[orderId]/loading.tsx
+++ app/checkout/[orderId]/loading.tsx
@@ -14,7 +14,7 @@ const Loading = () => {
         Order Received
       </h1>
       <h1 className="pt-8 text-xl px-6 self-center ">
-        we have received your order and we'll contact you soon.
+        we have received your order and we will contact you soon.
       </h1>
       <div className="flex flex-col items-center justify-center  h-fit mb-20 mt-10 mx-4 gap-8 bg-primary-foreground  lg:p-10 p-3 rounded-xl shadow-md ">
         <h1 className="text-xl font-semibold mt-3">Order Information</h1>
--- app/checkout/[orderId]/page.tsx
+++ app/checkout/[orderId]/page.tsx
@@ -35,7 +35,7 @@ const Page = ({ params }: { params: { orderId: string } }) => {
         Order Received
       </h1>
       <h1 className="pt-8 text-xl px-6 self-center ">
-        we have received your order and we'll contact you soon.
+        we have received your order and we will contact you soon.
       </h1>
       <div className="flex flex-col items-center justify-center  h-fit mb-20 mt-10 mx-4 text-center gap-8 bg-primary-foreground  lg:p-10 p-3 rounded-xl shadow-md ">
         <h1 className="text-xl font-semibold mt-3">Order Information</h1>
